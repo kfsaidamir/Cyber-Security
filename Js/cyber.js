7c2b80aeeaf3b51761d93ded1758fed14fae3d89
kf_saidamir <skadirov2009@gmail.com>
2024-03-26 23:57:26 +0500

    S

--- Js/cyber.js
+++ Js/cyber.js
@@ -1,5 +1,12 @@
 const back = document.querySelector('button')
+const btn = document.querySelector('.cyber__info > button')
 
 back.addEventListener('click' , () => {
     window.location.replace('index.html')
+    alert('One second!')
 } )
+
+btn.addEventListener('click' , () => {
+    prompt('Do you like it?')
+} )
+
