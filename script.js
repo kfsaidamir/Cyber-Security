a909d60d09771ef06c5d99adef87f8e11d1c345a
kf_saidamir <skadirov2009@gmail.com>
2024-03-26 22:37:37 +0500

    S

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+// require('dotenv').config();
+// const node = require('nodemailer');
+//  const transporter = {
+//     service : 'gmail',
+//     auth: {
+//         user: process.env.EMAIL,
+//         pass : process.env.PASSWORD,
+//     }
+//  };
+
+
+//  const mailOptions = {
+//     from : 'skadirov2009@gmail.com',
+//     to: 'skadirov2009@gmail.com',
+//     subject : 'From node js',
+//     text : 'thank you for your regestation'
+//  };
+//  transporter.sendMail(mailOptions);
+
 const a = document.querySelector('input')
 const btn = document.querySelector('.regestration__log > button')
+const btn2 = document.querySelector('.regestration__info > button')
 
 btn.addEventListener('click', () => {
     alert('Thank you for regestration!') 
+   location.reload()
+   console.log('hello wolrd')
 } )
 
-const btn2 = document.querySelector('.regestration__info > button')
 btn2.addEventListener('click' , (e) => {
-    e.target
     window.location.replace('https://myaccount.google.com/personal-info');
 } )
+
+
+
